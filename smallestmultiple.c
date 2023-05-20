78b4430d9fb961f237c80eae5af2bb7835386fe8
Priyanshu Prasad Gupta <palladium@pop-os.localdomain>
2023-05-20 09:47:49 +0530

    fvdfv

--- smallestmultiple.c
+++ smallestmultiple.c
@@ -11,8 +11,7 @@ int isDivisible1to20(int n) {
 
 int main() {
     int n = 1;
-    while (isDivisible1to20) {
-        printf("%d is not visible\n", n);
+    while (isDivisible1to20(n) != 1) {
         n++;
     }
     printf("%d\n", n);
